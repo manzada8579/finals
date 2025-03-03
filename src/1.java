f4ef39c33d74df3a638837a8da3588fc80547769
manzada8579 <man_zada@gmx.com>
2025-03-03 13:45:04 +0000

    Add 1.java

---
+++ src/1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Code {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        for (int i : nums) {
+            System.out.println(i);
+        }
+    }
+}
