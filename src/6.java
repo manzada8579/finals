7e474fbfc26992a4f56a9ee9735d71e3d1fd0a62
manzada8579 <man_zada@gmx.com>
2025-03-08 18:35:52 +0000

    Add 6.java

---
+++ src/6.java
@@ -0,0 +1,9 @@
+
+public class RandomJavaCode {
+    public static void main(String[] args) {
+        int x = 5;
+        String y = "hello";
+        System.out.println("The value of x is: " + x);
+        System.out.println("The value of y is: " + y);
+    }
+}
